b1ad863af56d98a7d3883ddf47d1cf12c1612dc7
Psithurial <96632893+Psithurial@users.noreply.github.com>
2022-01-04 18:40:52 -0500

    Create WoTColony.js
    Colony class original

---
+++ WoTColony.js
@@ -0,0 +1,9 @@
+class Colony{
+  constructor(name, product){
+    this._name = name;
+    this._product = product;
+  }
+
+  get name(){return this._name};
+  get product(){return this._product};
+}
