2ff0d58b3919489c2ef46697e432408d28481293
Psithurial <96632893+Psithurial@users.noreply.github.com>
2022-01-05 14:09:22 -0500

    Update WoTResource.js
    resource values almost complete

--- WoTResource.js
+++ WoTResource.js
@@ -1,10 +1,11 @@
 class Resource{
-  constructor(name, products, productionR, producers, consumers){
+  constructor(name, products, producers, consumptionR, consumers, nomVal){
     this._name = name;
     this._products = products;
-    this._productionR = productionR;
     this._producers = producers;
+    this._consumptionR = consumptionR;
     this._consumers = consumers;
+    this._nomVal = nomVal;
     this._maxPrice = 0;
     this._minPrice = 0;
     this._maxSell = 0;
@@ -13,9 +14,10 @@ class Resource{
 
   get name(){return this._name};
   get products(){return this._products};
-  get productionR(){return this._productionR};
   get producers(){return this._producers};
-  get consumers(){return this._consumers}
+  get consumptionR(){return this._consumptionR}
+  get consumers(){return this._consumers};
+  get nomVal(){return this._nomVal};
   get maxPrice(){return this._maxPrice};
   get minPrice(){return this._minPrice};
   get maxSell(){return this._maxSell};
@@ -28,8 +30,8 @@ class Resource{
 }
 
 class ManufacturedGood extends Resource{
-  constructor(name, factors, products, productionR, producers, consumers){
-    super(name, products, productionR, producers, consumers);
+  constructor(name, factors, products, producers, consumptionR, consumers, nomVal){
+    super(name, products, producers, consumptionR, consumers, nomVal);
     this._factors = factors;
   }
   
@@ -45,7 +47,7 @@ Estimated value: $ 20
 Can be replaced with: Wood
 cons rate: 1/33333
 */
-const coal = new Resource('coal', [], 1, [coalMine, miningCamp], [cities]);
+const coal = new Resource('coal', [], [coalMine, miningCamp], (1/33333), [cities]);
 
 
 /*Coffee
@@ -53,74 +55,113 @@ Produced by: Coffee Farm
 Consumed by: Cities
 Estimated value: $ 40
 cons rate: 1/20000
+*/
+const coffee = new Resource('coffee', [], [coffeeFarm], (1/20000), [cities]);
 
+/*
 Copper
 Produced by: Copper Mine
 Consumed by: Mint
 Estimated value: $ 40
-
+*/
+ const copper = new Resource('copper', [], [copperMine], 0, [mint]);                           
+                            
+/*
 Cotton
 Produced by: Cotton Farm, Cotton Plantation Colony
 Consumed by: Textile Mill
 Estimated value: $ 20
+*/
+const cotton = new Resource('cotton', [],[cottonFarm, cottonPlantation], 0, [textileMill]);
 
+/*
 Fish
 Produced by: Fishery
 Consumed by: Cities
 Estimated value: $ 15
 Can be replaced with: Grain
 cons rate: 1/3333
+*/
+const fish = new Resource ('fish', [], [fishery], (1/3333), [cities]);
 
+/*
 Gold Ore
 Produced by: Gold Mine
 Consumed by: Gold Smelter
 Estimated value: $ 200
-
+*/
+const goldOre = new Resource('goldOre', [], [goldMine], 0, [goldSmelter]);
+                           
+/*
 Grain
 Produced by: Grain Farm
 Consumed by: Cities
 Estimated value: $ 10
 Can be replaced with: Fish
-
+*/
+const grain = new Resource('grain', [], [grainFarm], (1/769), [cities]);
+                           
+/*
 Iron
 Produced by: Iron Mine, Mining Camp Colony
 Consumed by: Blacksmith, Arsenal
 Estimated value: $ 25
-
+*/
+const iron = new Resource('iron', [], [ironMine, MiningCamp], 0, [blacksmith, arsenal]);
+                           
+/*
 Ivory
 Produced by: Poachers' Den
 Consumed by: Jewelry Factory
 Estimated value: $ 300
-
+*/
+const ivory = new Resource('ivory', [], [poachersDen], 0, [jewelryFactory]);
+                           
+/*
 Opium
 Produced by: Opium Farm
 Consumed by: Opium Den
 Estimated value: $ 500
 May be illegal in some countries
-
+*/
+const opium = new Resource('opium', [], [opiumFarm], 0, [opiumDen]);
+                           
+/*
 Silk
 Produced by: Silk Plantation
 Consumed by: Weaving Mill
 Estimated value: $ 110
-
+*/
+const silk = new Resource('silk', [], [silkPlantation], 0, [weavingMill]);
+                           
+/*
 Sugar
 Produced by: Sugar Farm, Sugar Plantation Colony
 Consumed by: Cities, Distillery
 Estimated value: $ 20
 cons rate: 1/2500
-
+*/
+const sugar = new Resource('sugar', [], [sugarFarm, sugarPlantation], (1/2500), [cities, distillery]);
+                           
+/*
 Tea
 Produced by: Tea Plantation
 Consumed by: Cities
 Estimated value: $ 100
 cons rate: 1/10000
-
+*/
+const tea = new Resource('tea', [], [teaPlantation], (1/10000), [cities]);
+                           
+/*
 
 Tobacco
 Produced by: Tobacco Farm, Tobacco Plantation Colony
 Consumed by: Cigar Factory
 Estimated value: $ 35
-
+*/
+const tobacco = new Resource('tobacco', [], [tobaccoFarm, tobaccoPlantation], 0, [cigarFactory]);
+                           
+/*
 
 
 
@@ -130,7 +171,10 @@ Consumed by: Cities, Arsenal, Blacksmith, Paper Mill, Shipyard
 Estimated value: $ 10
 Can be replaced with: Coal
 cons rate: 1/3333
-
+*/
+const wood = new Resource('wood', [], [loggingCamp, lumberCamp], (1/3333), [cities, arsenal, blacksmith, paperMill, shipyard]);
+                           
+/*
 Manufactured Goods
 
 Arms
@@ -140,7 +184,7 @@ Estimated value: $ 250
 Made with: 2x Iron, 1x Wood
 
 */
-const arms = new ManufacturedGood('arms', [iron, iron, wood], [], 1, [arsenal], [barracks]);                          
+const arms = new ManufacturedGood('arms', [iron, wood], [], [arsenal], 0, [barracks]);                          
 
 /*
 Beer
@@ -149,7 +193,10 @@ Consumed by: Cities
 Estimated value: $ 70
 Made with: 3x Grain
 cons rate: 1/25000
-
+*/
+const beer = new ManufacturedGood('beer', [grain], [], [brewery], (1/25000), [cities]);
+                           
+/*
 
 
 Books
@@ -158,7 +205,10 @@ Consumed by: Cities
 Estimated value: $ 150
 Made with: 2x Paper
 cons rate: 1/50000
-
+*/
+const books = new ManufacturedGood('books', [paper], [], [printingPress], (1/50000), [cities]);
+                           
+/*
 
 
 Cigars
@@ -168,7 +218,10 @@ Estimated value: $ 230
 Made with: 2x Tobacco, 1x Paper
 May be illegal in some countries
 cons rate: 1/20000
-
+*/
+const cigars = new ManufacturedGood('cigars', [paper], [], [cigarFactory, tobaccoPlantation], (1/20000), [cities]);
+                           
+/*
 
 Clothes
 Produced by: Clothing Factory, Cotton Plantation Colony (level 3)
@@ -176,7 +229,10 @@ Consumed by: Cities
 Estimated value: $ 150
 Made with: 1x Fabric
 cons rate: 1/10000
-
+*/
+const clothes = new ManufacturedGood('clothes', [fabric], [], [clothingFactory, cottonPlantation], (1/10000), [cities]);
+                           
+/*
 
 
 Fabric
@@ -185,15 +241,21 @@ Consumed by: Cities, Clothing Factory, Shipyard
 Estimated value: $ 80
 Made with: 2x Cotton
 cons rate: 1/10000
-
+*/
+const fabric = new ManufacturedGood('fabric', [cotton], [clothes], [textileMill, cottonPlantation], (1/10000), [cities, clothingFactory, shipyard]);
+                           
+/*
 
 
 Jewelry
 Produced by: Jewelry Factory
 Consumed by: Cities
 Estimated value: $ 1800
-Made with: 1x Gold Ore, 1x Ivory
-
+Made with: 1x Gold, 1x Ivory
+*/
+const jewelry = new ManufacturedGood('jewelry', [gold, ivory], [], [jewelryFactory], 0, [cities]);
+                           
+/*
 
 
 Gold
@@ -201,7 +263,10 @@ Produced by: Gold Smelter
 Consumed by: Jewelry Factory
 Estimated value: $ 1000
 Made with: 4x Gold Ore
-
+*/
+const gold = new ManufacturedGood('gold', [goldOre], [], [goldSmelter], 0, [jewelryFactory]);
+                           
+/*
 
 
 Paper
@@ -210,7 +275,10 @@ Consumed by: Cities, Cigar Factory, Printing Press
 Estimated value: $ 60
 Made with: 2x Wood
 cons rate: 1/50000
-
+*/
+const paper = new ManufacturedGood('paper', [wood], [cigars, books], [paperMill, lumberCamp], (1/50000), [cities, cigarFactory, printingPress]);
+                           
+/*
 
 
 Rum
@@ -220,7 +288,10 @@ Estimated value: $ 160
 Made with: 3x Sugar
 May be illegal in some countries
 cons rate: 1/20000
-
+*/
+const rum = new ManufacturedGood('rum', [sugar], [], [distillery, sugarPlantation], (1/20000), [cities]);
+                           
+/*
 
 Tools
 Produced by: Blacksmith, Mining Camp Colony (level 3)
@@ -228,3 +299,6 @@ Consumed by: Cities
 Estimated value: $ 90
 Made with: 1x Iron, 1x Wood
 cons rate: 1/100000
+*/
+const tools ManufacturedGood('tools', [iron, wood], [], [blacksmith, miningCamp], (1/100000), [cities]);
+                           
